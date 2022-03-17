05993d13d6d39bc2b14821458b241ce7f90ba42f
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-03-17 16:46:47 +0300

    Added the ID photo holder, the landing dashboard and its bottom navigation.

--- app/src/main/java/com/example/uidesigns/activities/MainActivity.kt
+++ app/src/main/java/com/example/uidesigns/activities/MainActivity.kt
@@ -6,11 +6,11 @@ import android.os.Bundle
 import android.os.Handler
 import com.example.uidesigns.R
 import com.example.uidesigns.onboarding.OnboardingScreenOneFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
     }
 }
