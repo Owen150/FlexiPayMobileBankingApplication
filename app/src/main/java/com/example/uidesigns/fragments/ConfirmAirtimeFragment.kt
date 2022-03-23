d7d9e6e629fcdd2245f0e4ae2349e93ce82c3fb8
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-03-24 02:11:43 +0300

    General Updates to the UI

--- app/src/main/java/com/example/uidesigns/fragments/ConfirmAirtimeFragment.kt
+++ app/src/main/java/com/example/uidesigns/fragments/ConfirmAirtimeFragment.kt
@@ -5,29 +5,19 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.findNavController
 import com.example.uidesigns.R
+import com.example.uidesigns.databinding.FragmentConfirmAirtimeBinding
+import com.example.uidesigns.databinding.FragmentInviteBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [ConfirmAirtimeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class ConfirmAirtimeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
+
+    private lateinit var _binding : FragmentConfirmAirtimeBinding
+    private val binding get() = _binding!!
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
+
     }
 
     override fun onCreateView(
@@ -35,26 +25,15 @@ class ConfirmAirtimeFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_confirm_airtime, container, false)
+        _binding = FragmentConfirmAirtimeBinding.inflate(inflater, container, false)
+        val view = binding.root
+        return view
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment ConfirmAirtimeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            ConfirmAirtimeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        binding.btncancel.setOnClickListener {
+            view.findNavController().navigate(R.id.action_confirmAirtimeFragment_to_buyAirtimeFragment)
+        }
     }
 }
