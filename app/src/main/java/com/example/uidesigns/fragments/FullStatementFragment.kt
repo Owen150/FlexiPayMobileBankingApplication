dbc946789634da221ce0ff407a2affeb2730cef4
Michael Owen Oduor <michaeloduor50@gmail.com>
2022-03-29 12:45:11 +0300

    Initialized the full statement fragment.

---
+++ app/src/main/java/com/example/uidesigns/fragments/FullStatementFragment.kt
@@ -0,0 +1,33 @@
+package com.example.uidesigns.fragments
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.example.uidesigns.R
+import com.example.uidesigns.databinding.FragmentFullStatementBinding
+
+class FullStatementFragment : Fragment() {
+
+    private lateinit var binding : FragmentFullStatementBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentFullStatementBinding.inflate(inflater, container,false)
+        val view = binding.root
+        return view
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
